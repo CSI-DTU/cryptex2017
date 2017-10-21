ffc4e57a19c1ee8c65d2f4d3592355cf90d04794
prince2398 <princepiyush2398@gmail.com>
2017-10-21 14:51:16 +0530

    App going to deploy on heroku

--- core.php
+++ core.php
@@ -3,7 +3,7 @@ define("DBHOST",     "localhost");
 define("DBUSER",     "root");
 define("DBPASSWORD",    "Prince2398");
 define("DBNAME",     "cryptex17");
-define("REDIRECTLINK", "http://localhost/cryptex2017");
+define("REDIRECTLINK", "https://cryptex2017.herokuapp.com/");
 define("LOGINLINK",REDIRECTLINK."/vide/examples/fb.php");
 define("APPLINK","https://www.facebook.com/cryptex2017/app/202980683107053/");
 define("DTUSITE", "http://csidtu.tech/");
