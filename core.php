df3bf00addeab17ad9b9dfdbbdc457b6d4675d2b
prince2398 <princepiyush2398@gmail.com>
2018-02-01 22:56:22 +0530

    server changes

--- core.php
+++ core.php
@@ -1,9 +1,9 @@
 <?php
 define("DBHOST",     "localhost");
 define("DBUSER",     "root");
-define("DBPASSWORD",    "Prince2398");
-define("DBNAME",     "cryptex17");
-define("REDIRECTLINK", "http://localhost/cryptex2017");
+define("DBPASSWORD",    "abcd1234");
+define("DBNAME",     "cryptex18");
+define("REDIRECTLINK", "http://139.59.87.249");
 define("LOGINLINK",REDIRECTLINK."/vide/examples/fb.php");
 define("APPLINK","https://www.facebook.com/cryptex2017/app/202980683107053/");
 define("DTUSITE", "http://csidtu.tech/");
