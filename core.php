479e8cd2cfb8c0d04d6da1f6f929a01ec9c39e15
prince2398 <prince_bt2k16@dtu.ac.in>
2018-02-10 07:22:58 +0000

    link changes

--- core.php
+++ core.php
@@ -3,9 +3,9 @@ define("DBHOST",     "localhost");
 define("DBUSER",     "root");
 define("DBPASSWORD",    "abcd1234");
 define("DBNAME",     "cryptex18");
-define("REDIRECTLINK", "http://139.59.87.249");
+define("REDIRECTLINK", "http://cryptex18.tech");
 define("LOGINLINK",REDIRECTLINK."/vide/examples/fb.php");
-define("APPLINK","https://www.facebook.com/cryptex2017/app/202980683107053/");
+define("APPLINK","https://www.facebook.com/csi.dtu");
 define("DTUSITE", "http://csidtu.tech/");
-define("COGENESISSITE", "http://cogenesis.dtu.ac.in/");
+define("COGENESISSITE", "http://invictusdtu.org/");
 ?>
