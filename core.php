894dbb8ffa6b704987503f1df04e982888cc47a2
prince2398 <princepiyush2398@gmail.com>
2019-02-01 14:00:47 +0000

    deploy

--- core.php
+++ core.php
@@ -1,13 +1,13 @@
 <?php
 define("DBHOST",        "localhost");
 define("DBUSER",        "root");
-define("DBPASSWORD",    "abcd1234");
+define("DBPASSWORD",    "cryptex2k19");
 define("DBNAME",        "cryptex");
 define("FBAPPID",       "565461693784792");
 define("FBAPPSECRET",   "c89075376defb090dd796342027a614f");
 
-define("REDIRECTLINK",  "http://cryptex.tech");
-define("LOGINLINK",     "https://cryptex.tech/vide/examples/login.php");
+define("REDIRECTLINK",  "http://cryptex19.ml");
+define("LOGINLINK",     "http://cryptex19.ml/vide/examples/login.php");
 define("APPLINK",       "http://www.facebook.com/dtu.csi");
 define("DTUSITE",       "http://csidtu.ml/");
 define("COGENESISSITE", "http://invictusdtu.org/");
