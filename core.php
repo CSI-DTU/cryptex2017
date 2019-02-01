48fc3ac2053440116b8ad45d1c05c8154a8e5235
prince2398 <princepiyush2398@gmail.com>
2019-02-01 19:36:08 +0530

    removed credentials

--- core.php
+++ core.php
@@ -3,8 +3,8 @@ define("DBHOST",        "localhost");
 define("DBUSER",        "root");
 define("DBPASSWORD",    "abcd1234");
 define("DBNAME",        "cryptex");
-define("FBAPPID",       "565461693784792");
-define("FBAPPSECRET",   "c89075376defb090dd796342027a614f");
+define("FBAPPID",       "");
+define("FBAPPSECRET",   "");
 
 define("REDIRECTLINK",  "http://cryptex.tech");
 define("LOGINLINK",     "https://cryptex.tech/vide/examples/login.php");
